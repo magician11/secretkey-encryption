5585a519daf8bae9f25a062379a962f84aa8d49e
Andrew Golightly <andrew@golightlyplus.com>
2016-03-31 22:18:19 +1100

    encoded secret key using tweetnacl

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const chloride = require('chloride/small');
+const tweetnacl = require('tweetnacl');
 const scrypt = require('scrypt-async');
-const crypto = require('crypto');
 
 const dkLen = 32;
 const interruptStep = 0;
+const logN = 16;
+const blockSize = 8;
 
 function encryptSecretKey(password, secretKey, callback) {
-  const salt = crypto.randomBytes(32);
+  const salt = tweetnacl.randomBytes(32);
   const nonce = new Uint8Array(24);
-  const logN = 16;
-  const blockSize = 8;
   scrypt(password, salt, logN, blockSize, dkLen, interruptStep, (derivedKey) => {
-    const encryptedSecretKey = chloride.crypto_secretbox(secretKey, nonce, new Uint8Array(derivedKey));
-    callback({ salt, logN, blockSize, encryptedSecretKey });
+    const encryptedSecretKey = tweetnacl.secretbox(secretKey, nonce, new Uint8Array(derivedKey));
+    callback({ salt, nonce, encryptedSecretKey });
   });
 }
 
-module.exports = {
-  encryptSecretKey
-};
+function decryptEncryptedSecretKey(password, encryptedSecretKeyBundle, callback) {
+  scrypt(password, encryptedSecretKeyBundle.salt, logN, blockSize, dkLen, interruptStep, (derivedKey) => {
+    const secretKey = tweetnacl.secretbox.open(encryptedSecretKeyBundle.encryptedSecretKey, encryptedSecretKeyBundle.nonce, new Uint8Array(derivedKey));
+    return callback(secretKey);
+  });
+}
+
+module.exports = { encryptSecretKey, decryptEncryptedSecretKey };
--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
 const tweetnacl = require('tweetnacl');
+tweetnacl.util = require('tweetnacl-util');
 const secretkeyEncryption = require('../index');
 
 const keyPair = tweetnacl.sign.keyPair();
+const encodedSecretKey = tweetnacl.util.encodeBase64(keyPair.secretKey);
+console.log(`Original secret key\n${encodedSecretKey}\n`);
+
 const password = 'test password';
 
-secretkeyEncryption.encryptSecretKey(password, keyPair.secretKey, function(encryptedSecretKeyBundle) {
-  console.log(JSON.stringify(encryptedSecretKeyBundle));
+secretkeyEncryption.encryptSecretKey(password, keyPair.secretKey, (encryptedSecretKeyBundle) => {
+  console.log(`Encrypted Secret Key Bundle (as JSON string)\n${JSON.stringify(encryptedSecretKeyBundle)}\n`);
+  secretkeyEncryption.decryptEncryptedSecretKey('test password', encryptedSecretKeyBundle, (secretKey) => {
+    if (secretKey === false) {
+      console.log('Error! Decryption of secret key failed.');
+    } else {
+      console.log(`Decrypted secret key\n${tweetnacl.util.encodeBase64(secretKey)}\n`);
+      console.log('Success! Secret successfully decrypted!');
+    }
+  });
 });
